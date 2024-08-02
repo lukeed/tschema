2ebc4ac79fe18d4d0368aaed6a26e723a6ece34f
Luke Edwards <luke.edwards05@gmail.com>
2024-08-02 12:12:56 -0700

    chore: minify via terser for gzip size

--- scripts/gzip.ts
+++ scripts/gzip.ts
@@ -1,17 +1,35 @@
 import { resolve } from 'jsr:@std/path';
+import { minify } from 'npm:terser@~5';
 
 // NOTE: run after "build" task
 let input = resolve('npm/index.mjs');
-let content = await Deno.readFile(input);
+let text = await Deno.readTextFile(input);
+
+let m = await minify(text, {
+	ecma: 2020,
+	mangle: true,
+	compress: true,
+	module: true,
+});
+
+if (m.code) {
+	console.log('\nMinified\n----------');
+	console.log(m.code + '\n');
+} else {
+	throw new Error('Error w/ Terser');
+}
 
 let bytes = {
-	raw: content.length,
+	raw: text.length,
+	min: m.code.length,
 	gzip: 0,
 };
 
 let stream = new ReadableStream({
 	start(ctrl) {
-		ctrl.enqueue(content);
+		ctrl.enqueue(
+			new TextEncoder().encode(m.code),
+		);
 		ctrl.close();
 	},
 }).pipeThrough(
@@ -28,7 +46,9 @@ while (true) {
 
 let raw = bytes.raw.toLocaleString();
 let gzip = bytes.gzip.toLocaleString();
+let min = bytes.min.toLocaleString();
 let max = Math.max(raw.length, gzip.length);
 
 console.log('bytes (raw): ', raw.padStart(max, ' '), 'b');
+console.log('bytes (min): ', min.padStart(max, ' '), 'b');
 console.log('bytes (gzip):', gzip.padStart(max, ' '), 'b');
