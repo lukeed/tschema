70f964fa258b2c5731700a602ee49b008a2a5ca6
Luke Edwards <luke.edwards05@gmail.com>
2024-10-12 04:10:49 -0700

    chore: update release script for deno@2

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,3 +1,5 @@
+// deno task release <version>
+
 import { assert } from 'jsr:@std/assert@^1.0';
 import { resolve } from 'jsr:@std/path@^1.0';
 
@@ -10,11 +12,10 @@ let content = await Deno.readTextFile(input);
 let config = JSON.parse(content);
 config.version = version;
 
-type Options = Parameters<typeof Deno.run>[0];
-async function run(label: string, options: Options) {
-	// deno-lint-ignore no-deprecated-deno-api
-	let p = await Deno.run(options).status();
-	assert(p.code === 0, label);
+async function run(label: string, options: Deno.CommandOptions) {
+	let bin = (options.args || []).shift();
+	let c = bin && await new Deno.Command(bin, options).output();
+	if (c) assert(c.code === 0, label);
 }
 
 content = JSON.stringify(config, null, 2);
@@ -22,21 +23,21 @@ await Deno.writeTextFile(input, content);
 
 // prevent CI error
 await run('deno fmt', {
-	cmd: ['deno', 'fmt', input],
+	args: ['deno', 'fmt', input],
 });
 
 await run('git add', {
-	cmd: ['git', 'add', input],
+	args: ['git', 'add', input],
 });
 
 await run('git commit', {
-	cmd: ['git', 'commit', '-m', `v${version}`],
+	args: ['git', 'commit', '-m', `v${version}`],
 });
 
 await run('git tag', {
-	cmd: ['git', 'tag', `v${version}`],
+	args: ['git', 'tag', `v${version}`],
 });
 
 await run('git push', {
-	cmd: ['git', 'push', 'origin', 'main', '--tags'],
+	args: ['git', 'push', 'origin', 'main', '--tags'],
 });
